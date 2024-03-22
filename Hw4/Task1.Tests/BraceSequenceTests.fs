69acab90b38586f932a79649b964a1bc5c8d020f
TimaFrolov <tima1911@yandex.ru>
2024-03-22 16:03:41 +0300

    Complete hw 4 task 1

---
+++ Hw4/Task1.Tests/BraceSequenceTests.fs
@@ -0,0 +1,15 @@
+module Hw4.Task1.Tests
+
+open NUnit.Framework
+open FsUnit
+
+let testCases =
+    [ "()(())", true
+      "([{}])", true
+      "([{]})", false
+      "((asdasdadsadsad)gfvsvsd)", true ]
+    |> List.map (fun (a, b) -> TestCaseData(a, b))
+
+[<TestCaseSource("testCases")>]
+let TestBraceChecker str expected =
+    checkBraceSequence str |> should equal expected
---
+++ Hw4/Task1/BraceSequence.fs
@@ -0,0 +1,30 @@
+﻿module Hw4.Task1
+
+let bracePairs =
+    seq {
+        ('(', ')')
+        ('[', ']')
+        ('{', '}')
+    }
+
+let isClosingBrace x =
+    bracePairs |> Seq.map snd |> Seq.contains x
+
+let isOpeningBrace x =
+    bracePairs |> Seq.map fst |> Seq.contains x
+
+let isBracePair p = bracePairs |> Seq.contains p
+
+let checkBraceSequence str =
+    let rec helper acc str =
+        if Seq.isEmpty str then
+            acc = []
+        else
+            let tail = Seq.tail str
+
+            match Seq.head str with
+            | x when isOpeningBrace x -> helper (x :: acc) tail
+            | x when isClosingBrace x -> isBracePair (List.head acc, x) && helper (List.tail acc) tail
+            | _ -> helper acc tail
+
+    helper [] str
