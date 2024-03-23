f227ff09dd5f359e8fffa0074be3653c2277611e
Timofey Frolov <tima1911@yandex.ru>
2024-03-23 09:37:03 +0300

    Complete hw 4 task 2 (#6)

---
+++ Hw4/Task2/PointFree.fs
@@ -0,0 +1,10 @@
+﻿module Hw4.Task2
+
+let func x l = List.map (fun y -> y * x) l
+// func x l = List.map (fun y -> y * x) l
+// func x = List.map (fun y -> y * x)
+// func x = List.map (fun y -> x * y) multiplication for integers is commutative
+// func x = List.map ((*) x)
+// func x = x |> (*) |> List.map
+// func = (*) >> List.map
+let func2 = (*) >> List.map
