b3fb962cc3324b8f6e5b223138d9ca391536664c
TimaFrolov <tima1911@yandex.ru>
2024-04-21 19:14:51 +0300

    Remove unnecessary Bind overload

--- Hw6/Hw6/String.fs
+++ Hw6/Hw6/String.fs
@@ -4,9 +4,6 @@ type Builder() =
     member this.Bind(x: string, f: bigint -> string option) : string option =
         let res = ref 0I in if bigint.TryParse(x, res) then f res.Value else None
 
-    member this.Bind(x: string option, f: bigint -> string option) : string option =
-        x |> Option.map (fun x -> this.Bind(x, f)) |> Option.flatten
-
     member this.Return(x: bigint) : string option = Some $"{x}"
 
 let calculate = Builder()
