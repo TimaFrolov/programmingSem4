9fbd327d3703c9b6a192056ec563e8f7241f7cc3
TimaFrolov <tima1911@yandex.ru>
2024-05-03 17:08:42 +0300

    Complete Hw 9 Task 1

---
+++ Hw9/Task1.Tests/LambdaParserTests.fs
@@ -0,0 +1,21 @@
+module Hw9.Task1.Tests
+
+open NUnit.Framework
+open FsUnit
+open FParsec
+
+let testCases =
+    [ "x", Eval(Var "x")
+      "let S = \\x y z.x z (y z)",
+      Let("S", Abs("x", Abs("y", Abs("z", App(App(Var "x", Var "z"), App(Var "y", Var "z"))))))
+      "let K = \\x y.x", Let("K", Abs("x", Abs("y", Var "x")))
+      "S K K", Eval(App(App(Var "S", Var "K"), Var "K"))
+      "let let = let", Let("let", Var "let")
+      "let", Eval(Var("let")) ]
+    |> List.map (fun (a, b) -> TestCaseData(a, b))
+
+[<TestCaseSource("testCases")>]
+let TestStatementParser test expected =
+    match run parseStatement test with
+    | Success(result, _, _) -> result |> should equal expected
+    | Failure(error, _, _) -> Assert.Fail($"Failed to parse: {error}")
