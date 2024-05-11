5c568fd7f94260862ceaebd2223c62a8dee4296f
Timofey Frolov <tima1911@yandex.ru>
2024-05-11 10:27:59 +0300

    Complete hw 4 task 1 (#7)

---
+++ Hw4/Task1/BraceSequence.fs
@@ -0,0 +1,27 @@
+﻿module Hw4.Task1
+
+let bracePairs =
+    seq {
+        ('(', ')')
+        ('[', ']')
+        ('{', '}')
+    }
+
+let isClosingBrace x =
+    bracePairs |> Seq.map snd |> Seq.contains x
+
+let isOpeningBrace x =
+    bracePairs |> Seq.map fst |> Seq.contains x
+
+let isBracePair p = bracePairs |> Seq.contains p
+
+let checkBraceSequence str =
+    let rec helper acc str =
+        match (Seq.tryHead str, acc) with
+        | None, x -> x = []
+        | Some x, _ when isOpeningBrace x -> helper (x :: acc) (Seq.tail str)
+        | Some x, h :: tl when isClosingBrace x && isBracePair (h, x) -> helper tl (Seq.tail str)
+        | Some x, _ when isClosingBrace x -> false
+        | Some _, _ -> helper acc (Seq.tail str)
+
+    helper [] str
