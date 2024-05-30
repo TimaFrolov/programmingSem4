3a1826d132239427ef19dd122c1dd060168b0085
TimaFrolov <tima1911@yandex.ru>
2024-05-30 21:39:11 +0300

    Make tests easier to read

--- hw5/Hw5.Tests/SimulatorTests.fs
+++ hw5/Hw5.Tests/SimulatorTests.fs
@@ -8,39 +8,86 @@ type OS =
     | Linux
 
 let testCases =
-    [ [ (Windows, Linux, 1.0); (Linux, Windows, 1.0) ],
-      [ (1, Windows); (2, Linux); (3, Linux) ],
-      [ 1, [ 2; 3 ]; 2, [ 1 ]; 3, [ 1 ] ],
-      [ 2 ],
+    [ { new NetworkInfo<OS, int> with // Everybody gets infected sooner or later
+          member _.getProbability start target =
+              match (start, target) with
+              | Windows, Linux -> 1.0
+              | Linux, Windows -> 1.0
+              | _ -> 0.0
+
+          member _.getOperatingSystem computer =
+              match computer with
+              | 1 -> Windows
+              | 2 -> Linux
+              | 3 -> Linux
+              | _ -> failwith "Invalid computer"
+
+          member _.getConnected computer =
+              match computer with
+              | 1 -> [ 2; 3 ]
+              | 2 -> [ 1 ]
+              | 3 -> [ 1 ]
+              | _ -> failwith "Invalid computer"
+
+          member _.infected() = [ 2 ]
+
+      },
       [ [ 1 ]; [ 3 ] ]
-      [ (Windows, Windows, 1.0); (Windows, Linux, 0.0); (Linux, Windows, 0.0) ],
-      [ (1, Windows); (2, Windows); (3, Linux) ],
-      [ 1, [ 2; 3 ]; 2, [ 1 ]; 3, [ 1 ] ],
-      [ 2 ],
-      [ [ 1 ] ] ]
-    |> List.map (fun (a, b, c, d, e) ->
-        ({ new NetworkInfo<OS, int> with
-            member _.getProbability start target =
-                a
-                |> Seq.choose (fun (x, y, z) -> if x = start && y = target then Some z else None)
-                |> Seq.head
+      { new NetworkInfo<OS, int> with // Some computers are infected and some are not because they have different opearting systems
+          member _.getProbability start target =
+              match (start, target) with
+              | Windows, Windows -> 1.0
+              | _ -> 0.0
 
-            member _.getOperatingSystem computer =
-                b
-                |> Seq.choose (fun (x, y) -> if x = computer then Some y else None)
-                |> Seq.head
+          member _.getOperatingSystem computer =
+              match computer with
+              | 1 -> Windows
+              | 2 -> Windows
+              | 3 -> Linux
+              | _ -> failwith "Invalid computer"
 
-            member _.getConnected computer =
-                c
-                |> Seq.choose (fun (x, y) -> if x = computer then Some y else None)
-                |> Seq.head
-                |> seq
+          member _.getConnected computer =
+              match computer with
+              | 1 -> [ 2; 3 ]
+              | 2 -> [ 1 ]
+              | 3 -> [ 1 ]
+              | _ -> failwith "Invalid computer"
 
-            member _.infected() = d
+          member _.infected() = [ 2 ]
 
-         },
-         e))
-    |> List.map (fun (a, e) -> TestCaseData(a, e))
+      },
+      [ [ 1 ] ]
+      { new NetworkInfo<OS, int> with // Some computers not connected with infected computers
+          member _.getProbability start target = 1.0
+
+          member _.getOperatingSystem computer =
+              match computer with
+              | 1 -> Windows
+              | 2 -> Windows
+              | 3 -> Linux
+              | 4 -> Windows
+              | 5 -> Linux
+              | 6 -> Linux
+              | 7 -> Linux
+              | _ -> failwith "Invalid computer"
+
+          member _.getConnected computer =
+              match computer with
+              | 1 -> [ 2; 3 ]
+              | 2 -> [ 1 ]
+              | 3 -> [ 1 ]
+              | 4 -> [ 5 ]
+              | 5 -> [ 4 ]
+              | 6 -> [ 7 ]
+              | 7 -> [ 6 ]
+              | _ -> failwith "Invalid computer"
+
+          member _.infected() = [ 2; 4 ]
+
+      },
+      [ [ 1; 5 ]; [ 3 ] ] ]
+    |> List.map (fun (network, expected) -> TestCaseData(network, expected))
+
 
 [<TestCaseSource("testCases")>]
 let TestSimulator (network: NetworkInfo<OS, int>) (expected: int list list) =
